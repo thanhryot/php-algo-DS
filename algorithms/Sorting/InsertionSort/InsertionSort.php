61b00fe8045549478fa598495730a3783d476d25
Nguyen Duc Thanh <ducthanh186nd@gmail.com>
2020-10-06 22:23:31 +0700

    add insertion sort

---
+++ algorithms/Sorting/InsertionSort/InsertionSort.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ThanhRyot\Algorithms\Sorting\InsertionSort;
+
+class InsertionSort
+{
+    public static function sort(array $arr)
+    {
+        for ($i = 1; $i < count($arr); $i++) {
+            $key = $arr[$i];
+            $j = $i - 1;
+
+            while ($j >= 0 && $arr[$j] > $key) {
+                $arr[$j + 1] = $arr[$j];
+                $j = $j - 1;
+            }
+            $arr[$j + 1] = $key;
+        }
+        return $arr;
+    }
+}
---
+++ test/Algorithms/Sorting/InsertionSort/InsertionSortTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Test\Algorithms\Sorting\InsertionSort;
+
+use PHPUnit\Framework\TestCase;
+use ThanhRyot\Algorithms\Sorting\InsertionSort\InsertionSort;
+
+class InsertionSortTest extends TestCase
+{
+    private $arr = [100, 12, 22, 50, 70, 80, 1, 4, 6];
+
+    public function testSorting()
+    {
+        $result = InsertionSort::sort($this->arr);
+        $this->assertSame([1, 4, 6, 12, 22, 50, 70, 80, 100], $result);
+    }
+
+}
